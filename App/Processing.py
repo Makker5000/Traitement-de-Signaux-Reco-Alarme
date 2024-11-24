3462464e6ed612b2d4c8b63438a06743d3b000e7
Insanytil <116179598+Insanytil@users.noreply.github.com>
2024-11-24 22:03:08 +0100

    Second Commit: Refactoring and removing useless lines and fonctions

--- App/Processing.py
+++ App/Processing.py
@@ -28,51 +28,6 @@ def combine_bandpass_filters(signal, fs, lowcuts, highcuts):
         filtered_signals.append(filtered_signal)
     return np.sum(filtered_signals, axis=0)
 
-# Fonction pour calculer la transformée de Fourier
-def compute_fft(signal, fs):
-    fft_result = np.abs(fft(signal))
-    freqs = np.fft.fftfreq(len(signal), 1 / fs)
-    return freqs[:len(freqs) // 2], fft_result[:len(freqs) // 2]
-
-# Fonction pour sauvegarder un fichier WAV
-def save_wav(file_path, fs, signal):
-    scaled_signal = np.int16(signal / np.max(np.abs(signal)) * 32767)
-    wavfile.write(file_path, fs, scaled_signal)
-
-# Fonction pour tracer les résultats
-def plot_results(time, original_signal, reconstructed_signal, freqs, fft_reconstructed):
-    plt.figure(figsize=(14, 10))
-
-    # Signal Original (Temporel)
-    plt.subplot(3, 1, 1)
-    plt.plot(time, original_signal, label="Signal Original", color="green")
-    plt.title("Signal Original (Temporel)")
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Amplitude")
-    plt.legend()
-
-    # Signal Reconstruit (Temporel)
-    plt.subplot(3, 1, 2)
-    plt.plot(time, reconstructed_signal, label="Signal Reconstruit", color="orange")
-    plt.title("Signal Reconstruit (Temporel)")
-    plt.xlabel("Temps (s)")
-    plt.ylabel("Amplitude")
-    plt.legend()
-
-    # Spectre Fréquentiel du Signal Reconstruit
-    plt.subplot(3, 1, 3)
-    plt.plot(freqs, fft_reconstructed, label="Spectre Reconstruit", color="blue")
-    plt.title("Spectre Fréquentiel (Signal Reconstruit)")
-    plt.xlabel("Fréquence (Hz)")
-    plt.ylabel("Amplitude")
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
-
-
-
-
 
 # Programme principal
 def Processing(chemin_fichier): # ,chemin_destination) : # ex : "../Ressources/Sons-De-Test/Hyper-chien.waw" && "../Output/son_traiter.waw"
@@ -86,19 +41,5 @@ def Processing(chemin_fichier): # ,chemin_destination) : # ex : "../Ressources/S
     # Appliquer les filtres et combiner les résultats
     signal_reconstructed = combine_bandpass_filters(signal, fs, lowcuts, highcuts)
 
-    # Calculer la transformée de Fourier
-    freqs, fft_reconstructed = compute_fft(signal_reconstructed, fs)
-
-    # Enregistrer le signal reconstruit dans un fichier WAV
-     
-            # output_file = chemin_destination
-            # save_wav(output_file, fs, signal_reconstructed)
-
-    # Tracer les résultats
-    time = np.linspace(0, len(signal) / fs, len(signal))
-    plot_results(time, signal, signal_reconstructed, freqs, fft_reconstructed)
-
-    # print(f"Le signal reconstruit a été enregistré dans le fichier : {output_file}")
-
-    return signal_reconstructed
+    return fs, signal_reconstructed
 
